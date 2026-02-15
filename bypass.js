9ccc12aaac3cc33a472f490bc54affda44c71bd6
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-02-15 13:26:28 +0100

    hhhhhhhhhhhh

--- bypass.js
+++ bypass.js
@@ -4,16 +4,24 @@
 // @homepage https://github.com/loadstring1/bypass-are-you-watching-youtube
 // @version 0.1
 // @description  Bypasses are you still watching
-// @match http://www.youtube.com/watch?v=*
+// @match *://*.youtube.com/*
 // @run-at document-end
+// @author loadstring1 - coza
 // @download-url https://raw.githubusercontent.com/loadstring1/bypass-are-you-watching-youtube/refs/heads/main/bypass.js
 // @update-url https://raw.githubusercontent.com/loadstring1/bypass-are-you-watching-youtube/refs/heads/main/bypass.js
+// @grant none
 // ==/UserScript==
 
 function ass(){
-    document.querySelector("#confirm-button").click(); 
-    console.log("clicked"); 
+    if (document.querySelector("#confirm-button")==null){
+        setTimeout(ass,1000)
+        return;
+    }
+    
+    document.querySelector("#confirm-button").click();
+    console.log("clicked");
     setTimeout(ass,1000)
-}; 
+};
 
+console.log("elo")
 ass()
