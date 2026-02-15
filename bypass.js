12a5a8ed373d678c2cd4faec8a6ba7a119b3f4f4
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-02-15 13:31:13 +0100

    ass

--- bypass.js
+++ bypass.js
@@ -9,8 +9,8 @@
 // @match *://*.music.youtube.com/*
 // @run-at document-end
 // @author loadstring1 - coza
-// @download-url https://raw.githubusercontent.com/loadstring1/bypass-are-you-watching-youtube/refs/heads/main/bypass.js
-// @update-url https://raw.githubusercontent.com/loadstring1/bypass-are-you-watching-youtube/refs/heads/main/bypass.js
+// @download-url https://github.com/loadstring1/bypass-are-you-watching-youtube/raw/refs/heads/main/bypass.js
+// @update-url https://github.com/loadstring1/bypass-are-you-watching-youtube/raw/refs/heads/main/bypass.js
 // @grant none
 // ==/UserScript==
 
