4874ba575ea748bacb8fc0c37a6c7e40111f15b7
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-02-18 19:10:39 +0100

    fhweuwegbf version

--- bypass.user.js
+++ bypass.user.js
@@ -2,7 +2,7 @@
 // @name YouTube Better NonStop
 // @namespace https://github.com/loadstring1/bypass-are-you-watching-youtube
 // @homepage https://github.com/loadstring1/bypass-are-you-watching-youtube
-// @version 1.3
+// @version 1.4
 // @description  Bypasses are you still watching
 // @match *://*.youtube.com/*
 // @match *://music.youtube.com/*
