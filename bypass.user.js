37166dd5569c251822e9aa773d1de5a4e496496f
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-02-21 14:55:17 +0100

    uncomment cause looks like hook got patched

--- bypass.user.js
+++ bypass.user.js
@@ -2,8 +2,8 @@
 // @name YouTube Better NonStop
 // @namespace https://github.com/loadstring1/bypass-are-you-watching-youtube
 // @homepage https://github.com/loadstring1/bypass-are-you-watching-youtube
-// @version 1.9.0.000
-// @description  Bypasses are you still watching
+// @version 1.9.0.6769
+// @description Bypasses are you still watching
 // @match *://*.youtube.com/*
 // @match *://music.youtube.com/*
 // @match *://*.music.youtube.com/*
@@ -15,62 +15,58 @@
 // @noframes
 // ==/UserScript==
 
-//old method - commented out bc its just wasting cpu cycles when event hook is better
-
-// var shouldKeepChecking=false
-// function keepVideoPlaying(vid){
-//     if (shouldKeepChecking==false) return;
-//     customLog("loop checking if youtube still kept the video paused")
-
-//     if (vid.paused){
-//         vid.play()
-//     }
-
-//     setTimeout(()=>{
-//         keepVideoPlaying(vid)
-//     },1000)
-// }
-
-// async function ass(){
-//     var video=document.querySelector("video")
-//     var shittyDialog=document.querySelector("tp-yt-paper-dialog")
-//     var confirmButton=shittyDialog && shittyDialog.querySelector("#confirm-button") || null
-
-//     if (video==null || shittyDialog==null || confirmButton==null){
-//         customLog("waiting for this stupid ass popup")
-//         setTimeout(ass,300)
-//         return;
-//     }
-
-//     if (window.getComputedStyle(shittyDialog).display=="none"){
-//         customLog("shitty popup is hidden. retrying...")
-//         setTimeout(ass,300)
-//         return;
-//     }
-
-//     customLog("detected shitty dialog - waiting 1 second")
-
-//     shouldKeepChecking=true
-//     keepVideoPlaying(video)
-
-//     await new Promise((resolve=>setTimeout(resolve,1000)))
-
-//     confirmButton.click()
-//     customLog("confirm button clicked.")
-
-//     setTimeout(()=>{
-//         shouldKeepChecking=false
-//         customLog("loop check killed.")
-//     },5000)
-
-//     setTimeout(ass,1000)
-// };
-
-//ass()
-
 function customLog(str){
     console.log(`better nonstop: ${str}`)
 }
+
+var shouldKeepChecking=false
+function keepVideoPlaying(vid){
+    if (shouldKeepChecking==false) return;
+    customLog("loop checking if youtube still kept the video paused")
+
+    if (vid.paused){
+        vid.play()
+    }
+
+    setTimeout(()=>{
+        keepVideoPlaying(vid)
+    },1000)
+}
+
+async function ass(){
+    var video=document.querySelector("video")
+    var shittyDialog=document.querySelector("tp-yt-paper-dialog")
+    var confirmButton=shittyDialog && shittyDialog.querySelector("#confirm-button") || null
+
+    if (video==null || shittyDialog==null || confirmButton==null){
+        customLog("waiting for this stupid ass popup")
+        setTimeout(ass,300)
+        return;
+    }
+
+    if (window.getComputedStyle(shittyDialog).display=="none"){
+        customLog("shitty popup is hidden. retrying...")
+        setTimeout(ass,300)
+        return;
+    }
+
+    customLog("detected shitty dialog - waiting 1 second")
+
+    shouldKeepChecking=true
+    keepVideoPlaying(video)
+
+    await new Promise((resolve=>setTimeout(resolve,1000)))
+
+    confirmButton.click()
+    customLog("confirm button clicked.")
+
+    setTimeout(()=>{
+        shouldKeepChecking=false
+        customLog("loop check killed.")
+    },5000)
+
+    setTimeout(ass,1000)
+};
 
 customLog(`started ${location.href}`)
 customLog(`hooking focus`)
@@ -88,3 +84,4 @@ Object.defineProperty(document, "hasFocus",{
 })
 
 customLog("focus hooked")
+ass()
